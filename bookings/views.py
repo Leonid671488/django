576f6e702e8de5fe21154d649a4d5ba790fe10d5
Leonid671488 <matigorovleonid@gmail.com>
2026-07-15 20:54:17 +0300

    Catalog added

--- bookings/views.py
+++ bookings/views.py
@@ -15,6 +15,11 @@ def catalog(request):
         "title": "Сеть моно-ресторанов | Выбор ресторана",
         "restaurants": [
             {"cuisine": "Рыба", "rating": 4.9, "address": "Приморский бульвар, д. 8", "image": "https://img.freepik.com/premium-photo/beautiful-old-bildings-marseille_948265-267099.jpg?semt=ais_hybrid"},
+            {"cuisine": "Рыба", "rating": 4.9, "address": "Приморский бульвар, д. 8", "image": "https://img.freepik.com/premium-photo/beautiful-old-bildings-marseille_948265-267099.jpg?semt=ais_hybrid"},
+            {"cuisine": "Гриль", "rating": 4.8, "address": "пр. Просвещения, д. 45", "image": "https://img.freepik.com/free-photo/luxury-dining-room-with-elegant-decor-lighting-generated-by-ai_24640-84695.jpg?semt=ais_hybrid"},
+            {"cuisine": "Гриль", "rating": 4.8, "address": "пр. Просвещения, д. 45", "image": "https://img.freepik.com/free-photo/luxury-dining-room-with-elegant-decor-lighting-generated-by-ai_24640-84695.jpg?semt=ais_hybrid"},
+            {"cuisine": "Гриль", "rating": 4.8, "address": "пр. Просвещения, д. 45", "image": "https://img.freepik.com/free-photo/luxury-dining-room-with-elegant-decor-lighting-generated-by-ai_24640-84695.jpg?semt=ais_hybrid"},
+            {"cuisine": "Гриль", "rating": 4.8, "address": "пр. Просвещения, д. 45", "image": "https://img.freepik.com/free-photo/luxury-dining-room-with-elegant-decor-lighting-generated-by-ai_24640-84695.jpg?semt=ais_hybrid"},
             {"cuisine": "Гриль", "rating": 4.8, "address": "пр. Просвещения, д. 45", "image": "https://img.freepik.com/free-photo/luxury-dining-room-with-elegant-decor-lighting-generated-by-ai_24640-84695.jpg?semt=ais_hybrid"},
             {"cuisine": "Азия", "rating": 5.0, "address": "ул. Набережная, д. 12", "image": "https://avatars.mds.yandex.net/i?id=50fcdc06bacd4542e321b8d5b0aff052_l-5254479-images-thumbs&n=13"},
         ]
