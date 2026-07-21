3f4bcf33d8ff041eb74cdb593e63173c27609cec
Leonid671488 <matigorovleonid@gmail.com>
2026-07-21 23:06:16 +0300

    database addad

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from bookings.models import *
+
+admin.site.register(RestaurantCategory)
+admin.site.register(Restaurant)
+admin.site.register(User)
+admin.site.register(Booking)
+admin.site.register(Feedback)
--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+class RestaurantCategory(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Restaurant(models.Model):
+    address = models.CharField(max_length=128, unique=True)
+    category = models.ForeignKey(RestaurantCategory, on_delete=models.CASCADE)
+    # rating, image
+
+    def __str__(self):
+        return f"{self.category.name} | {self.address}"
+
+
+class User(models.Model):
+    name = models.CharField(max_length=32)
+    password = models.CharField(max_length=64, unique=True)
+    email = models.EmailField()
+    phone = models.CharField(max_length=16)
+
+    def __str__(self):
+        return f"{self.name} | {self.phone}"
+
+
+class Booking(models.Model):
+    time = models.DateTimeField()
+    peoples = models.PositiveSmallIntegerField()
+    status = models.CharField(max_length=16, default="Простаивает")
+    booking_time = models.DateTimeField(default=datetime.datetime.now())
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.name} | {self.status}"
+
+
+class Feedback(models.Model):
+    mark = models.PositiveSmallIntegerField()
+    text = models.TextField()
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.booking.user.name} | {self.mark}"
